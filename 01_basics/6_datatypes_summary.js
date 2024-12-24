349c18ff690b0c603be8c94d03e4e49d7b96adf2
dhirenlulla <dhirenlulla18@gmail.com>
2024-12-24 18:26:05 +0000

    datatypes summary

---
+++ 01_basics/6_datatypes_summary.js
@@ -0,0 +1,19 @@
+// Primitie Datatypes ->
+
+// String, number, boolean, null, undefined, symbol, bigInt
+
+// Non-Primitive (Reference) Datatypes ->
+
+// Arrays, Objects, functions
+
+const goatsArrays = ["Virat Kohli", "Sachin Tendulkar", "Brian Lara"];
+
+let myObj = {
+    name: dhiren,
+    age: 19,
+    cgpa: 8.71,
+}
+
+const myFunction = function(){
+    console.log("Hello world!");
+}
