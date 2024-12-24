62f02b633af1861223dac0d1b137efe37ed36385
dhirenlulla <dhirenlulla18@gmail.com>
2024-12-24 16:22:42 +0000

    variables, datatypes and conversion in javascript

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const accountId = 34
+// const is used to declare constants that cannot be changed once declared
+
+let accountEmail = "dpl@gmail.com"
+// let keyword is used for declaring the variable, the value of variable is changable
+
+var accountPassword = 7576
+// var is also used for declaring the variable, 
+// but it is prefered not to use var because of problem
+// regarding block scope and functional scope
+
+accountCity = "maharashtra"
+// we can also declare a varibale without using let or var
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+// this is the better way to print the values of multiple variables in the form of table
+
