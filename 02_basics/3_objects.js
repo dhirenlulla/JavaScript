4e33650621c59f6952407beaea592c5910be2cbc
dhirenlulla <dhirenlulla18@gmail.com>
2025-01-10 14:32:27 +0000

    objects part 1.

--- 02_basics/3_objects.js
+++ 02_basics/3_objects.js
@@ -3,11 +3,15 @@
 // iinterview questions
 
 
-
 // object literals 
+
+// we declare symbols in a objects like this:
+const mySym = Symbol("key1");
 
 const JsUser = {
     name: "dhiren",
+    "full name": "dhiren lulla",
+    [mySym] : "myKey1", // here, symbols are declared in square brackets
     age: 18,
     email: "dhiren@google.com",
     location: "maharashtra",
@@ -19,10 +23,25 @@ const JsUser = {
 
 // using dot ( . )
 console.log(JsUser.email);
-console.log(JsUser["email"]);
+console.log(JsUser["full name"]); // this method is used for accessing the properties of object like "full name"
 
 // we can change value of an object
 JsUser.email = "dhirenchatgpt.com"
 // if we do not want to let values to be changed in the object, use keyword 'freeze'
-Object.freeze(JsUser)
-JsUser.email = "dhiren@microsoft.com" // this will not be updated after we have freezed the object
+// Object.freeze(JsUser)
+// JsUser.email = "dhiren@microsoft.com" // this will not be updated after we have freezed the object
+
+console.log(JsUser)
+
+// we can declare functions too in  object -->
+JsUser.greeting = function(){
+    console.log("Hello, good to see you :)");
+}
+// if we want to use the objects properties in the function, we would do this way ->
+JsUser.greetingTwo = function(){
+    console.log(`Hello, good to see you ${this.name}`); // we use backticks in the function, and with dollar sign and curly braces we can access the objects
+    // properties by using "this" keyword.
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
